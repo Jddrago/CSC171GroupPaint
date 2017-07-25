9dab649a238d730f26d21bca8cbea362e18b0780
klee <knlee6@gmail.com>
2017-07-24 20:38:06 -0600

    Added menu for shapes

--- Paint/app/src/main/java/com/example/jason/paint/MainActivity.java
+++ Paint/app/src/main/java/com/example/jason/paint/MainActivity.java
@@ -2,6 +2,9 @@ package com.example.jason.paint;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,4 +13,29 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu){
+        MenuInflater menuInflater = getMenuInflater();
+        menuInflater.inflate(R.menu.menu, menu);
+        return true;
+    }
+    public boolean onOptionsItemSelected(MenuItem item){
+        switch(item.getItemId()){
+            case R.id.arrow:
+                break;
+            case R.id.circle:
+                break;
+            case R.id.heart:
+                break;
+            case R.id.hexagon:
+                break;
+            case R.id.square:
+                break;
+            case R.id.star:
+                break;
+            default: return super.onOptionsItemSelected(item);
+        }
+        return true;
+    }
+
 }
